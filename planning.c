2a30349d56ebcaecca325b1d71bb1420047b1eb4
Justin <justin@justintimetechnology.com>
2013-09-09 23:51:08 -0400

    code fixes

---
+++ planning.c
@@ -0,0 +1,12 @@
+/*Justin Newman
+CIS170 Section 01
+Program to calculate cost and water savings for
+city planning project.*/
+
+#include<stdio.h>
+
+int main()
+	{
+	
+		printf("Enter population");
+		scanf("%l"
