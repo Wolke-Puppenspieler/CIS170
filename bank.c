50817cc9eb8f5ffa6877a138f84d70fd76751b73
Justin <justin@justintimetechnology.com>
2013-10-06 23:12:37 -0400

    updates

--- bank.c
+++ bank.c
@@ -4,36 +4,69 @@ Bank Program*/
 
 #include<stdio.h>
 
-char dispayMenu();
+void displayMenu();
+double getBalance(double );
+double getWithdrawal(double );
+double getDeposit(double );
 
 int main()
 	{
 		char selection;
-		unsigned int balance;
-		
+		double balance;
+
 		do
 			{
-				printf("Welcome to HFCC Federal Credit Union");
-				printf("\n\nPlease select from the following menu:");
-		
-				selection=displayMenu();
-		
-				printf("\n\nPlease enter your selection now:");
-			}while(selection!=Q||q)
-		
+				displayMenu();
+
+				scanf("%c",&selection);
+
+				switch(selection)
+					{
+					case 'D':case 'd':
+						clrscr();
+						balance=getDeposit(balance);
+					break;
+					case 'W':case 'w':
+						clrscr();
+						balance=balance-getWithdrawal(balance);
+					break;
+					case 'B':case 'b':
+						clrscr();
+						printf("Your current balance is: $%.2lf",balance);
+					break;
+					case 'Q':case 'q':
+						printf("Thank you, come again");
+					break;
+					default :
+						printf("Please make a valid selection");
+					}
+
+			}while(selection!='Q'||selection!='q');
+
 		return 0;
-		
+
 	}
-	
-char displayMenu()
+
+void displayMenu()
 	{
-		char choice;
-		printf("\nD:\tMake a deposit");
+		printf("\nWelcome to HFCC Federal Credit Union");
+		printf("\n\nPlease select from the following menu:");
+		printf("\n\nD:\tMake a deposit");
 		printf("\nW:\tMake a withdrawal");
 		printf("\nB:\tCheck your account balance");
 		printf("\nQ:\tTo quit");
-		
-		scanf("%c",&choice);
-		
-		return(choice);
+		printf("\n\nPlease enter your selection now:");
 	}
+double getBalance(double balance)
+    {
+
+     return 0;
+    }
+double getDeposit(double balance)
+    {
+    return 0;
+    }
+double getWithdrawal(double balance)
+    {
+    return 0;
+    }
