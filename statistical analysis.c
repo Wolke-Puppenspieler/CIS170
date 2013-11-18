4434e9769a48723dcf13d7145efcc847f02da763
Justin <justin@justintimetechnology.com>
2013-11-17 20:18:51 -0500

    diag

--- statistical analysis.c
+++ statistical analysis.c
@@ -6,8 +6,7 @@ Justin Newman*/
 #define MAX 10
 void getData(int []);
 void sortData(int []);
-void calcStats(int [], float *, float *, float *, int *);
-void displayStats(int [], float *, float *, float *, int *);
+
 //main function
 int main()
 	{
@@ -32,10 +31,9 @@ int main()
 				printf("\n%d", nums[i]);
 				
 			}
-				
-		calcStats(nums, &median, &mode, &average, &range);
+
 		
-		printf("%d, %.2f,", range, average);
+
 		
 		getch();
 		return 0;
@@ -71,21 +69,4 @@ void sortData(int nums[MAX])
 					}
 			}
 	}
-//calculate statistics
-void calcStats(int nums[MAX], float *median, float *mode, float *avg, int *range)
-	{
-		int i=0, min=0, max=0, temp, tot=0;
-		for(i=0;i<9;i++)
-			{
-				if(max<=nums[i])
-				{max=nums[i];}
-				if(min>=nums[i])
-				{min=nums[i];}
-				tot=tot+nums[i];
-			}
-		*range=max-min;
-		*avg=tot/10.0;
-	
-		
-	}
-		
+
